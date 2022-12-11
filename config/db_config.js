f4e410821b11dcd6d2dbd76d351225e5193c206c
hariprasad36825 <hariprasad36825@gmail.com>
2022-12-11 22:31:08 +0530

    babel build added

--- config/db_config.js
+++ config/db_config.js
@@ -1,3 +1,13 @@
+import dotenv from 'dotenv'
 import Sequelize from 'sequelize'
+dotenv.config()
 
-export const sequelize = new Sequelize('sqlite::memory')
+const env = process.env.NODE_ENV
+
+export let sequelize
+
+if (env === 'test') {
+  sequelize = new Sequelize('sqlite::memory')
+} else {
+  sequelize = new Sequelize('sqlite3')
+}
