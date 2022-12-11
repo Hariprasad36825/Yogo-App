f57647fa3a570f50d8d8545ca9036e273f216f3c
hariprasad36825 <hariprasad36825@gmail.com>
2022-12-11 23:06:06 +0530

    change db config

--- config/db_config.js
+++ config/db_config.js
@@ -3,11 +3,18 @@ import Sequelize from 'sequelize'
 dotenv.config()
 
 const env = process.env.NODE_ENV
+const db = process.env.DB
+const username = process.env.username
+const password = process.env.password
 
 export let sequelize
 
 if (env === 'test') {
   sequelize = new Sequelize('sqlite::memory')
 } else {
-  sequelize = new Sequelize('sqlite3')
+  sequelize = new Sequelize(db, username, password, {
+    dialect: 'sqlite',
+    // we will be saving our db as a file on this path
+    storage: 'database.sqlite'
+  })
 }
