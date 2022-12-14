73b7c09475a0d94681ab0ad58ad60882269a6246
hariprasad36825 <hariprasad36825@gmail.com>
2022-12-14 09:54:40 +0530

    automated batch insertion on server refresh

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import app from './app'
 import { sequelize } from './config/db_config'
 import { Batch } from './models/batch.model'
 import { Enrollment } from './models/enrollment.model'
+import { createBatch } from './services/batch.services'
 dotenv.config()
 
 // Constants
@@ -15,7 +16,11 @@ sequelize.authenticate().then(async () => {
   // sync db
   Batch.hasMany(Enrollment)
   Enrollment.belongsTo(Batch, { foreignKey: 'batch' })
-  await sequelize.sync()
+  await sequelize.sync({ force: true })
+  await createBatch('6.00 - 7.00 AM')
+  await createBatch('7.00 - 8.00 AM')
+  await createBatch('9.00 - 10.00 AM')
+  await createBatch('5.00 - 6.00 PM')
   if (env === 'test') {
     app.listen(PORT, HOST)
     console.log(`connection establised on ${HOST}:${PORT}`)
